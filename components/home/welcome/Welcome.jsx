e0dd58da4958fe12d6bc39665feed2bfa353d709
manish-lunivatech <manish.himalaya@lunivatech.com>
2023-06-12 20:38:13 +0545

    adding welcome

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
 
-import styles from './welcome.style'
+import { icons, SIZES } from "../../../constants";
+import styles from "./welcome.style";
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Himal</Text>
+        <Text style={styles.welcomeMessage}>
+          Welcome Himal,Find your perfect job
+        </Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput} />
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
