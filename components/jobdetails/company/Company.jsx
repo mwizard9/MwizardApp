bbff055019e054e4c41d4ce58ab1fe69f21cf47e
mwizard9 <manishhimalaya64@gmail.com>
2023-07-16 22:20:01 +0545

    updating details of job page

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import { View, Text,Image } from 'react-native'
+import React from "react";
+import { View, Text, Image } from "react-native";
 
-import styles from './company.style'
-import { icons } from '../../../constants'
-import { checkImageURL } from '../../../utils'
+import styles from "./company.style";
+import { icons } from "../../../constants";
+import { checkImageURL } from "../../../utils";
 
-const Company = ({companyLogo, jobTitle, companyName, Location}) => {
+const Company = ({ companyLogo, jobTitle, companyName, location }) => {
   return (
     <View style={styles.container}>
-      <View style= {styles.logoBox}>
+      <View style={styles.logoBox}>
         <Image
-        source={{
-          uri: checkImageURL(companyLogo)
-          ? companyLogo
-          : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg"
-        }}
-        style={styles.logoImage}
+          source={{
+            uri: checkImageURL(companyLogo)
+              ? companyLogo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+          style={styles.logoImage}
         />
-
       </View>
+
       <View style={styles.jobTitleBox}>
         <Text style={styles.jobTitle}>{jobTitle}</Text>
       </View>
@@ -35,7 +35,7 @@ const Company = ({companyLogo, jobTitle, companyName, Location}) => {
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Company
+export default Company;
